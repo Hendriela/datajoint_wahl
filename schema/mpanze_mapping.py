ac3cffcd90c12a0f5ac995042661e66b16f1265b
Matteo <matteo.panzeri@gmail.com>
2021-11-08 18:25:45 +0100

    extend mapping pipeline

--- schema/mpanze_mapping.py
+++ schema/mpanze_mapping.py
@@ -56,3 +56,35 @@ class RawParameterFile(dj.Manual):
     ---
     filename_params              : varchar(512)  # name of the sync file, relative to the session folder
     """
+
+
+@schema
+class MappingInfo(dj.Computed):
+    definition = """ # Automatically computed parameters from the sesssion file
+    -> MappingSession
+    ---
+    pre_stim                        : float     # pre stimulus interval in seconds
+    stim_duration                   : float     # stimulus duration in seconds
+    post_stim                       : float     # post stimulus interval in seconds
+    repeats                         : int       # number of times the stimulus is delivered
+    stim_timestamps                 : longblob  # stimulation timestamps in seconds
+    sampling_rate                   : int       # Vi sampling rate
+    stop_time                       : float     # total duration of the session, in seconds
+    session                         : int       # counter for repeated sessions with the same body part
+    """
+
+
+class ProcessedMappingFile(dj.Computed):
+    definition = """ # Computes and saves aligned,smoothed, pixelwise DF/F as a tiff file.
+    -> MappingInfo
+    ---
+    n_frames_pre                    : int        # number of pre stim (baseline) frames
+    n_frames_stim                   : int        # number of frames while the stimulus is active
+    n_frames_post                   : int        # number of frames post stimulation
+    n_frames_trial                  : int        # total number of frames in a trial
+    -> mpanze_widefield.SmoothingKernel
+    imaging_fps                     : float      # detected frames per second from sync path
+    frame_timestamps                : longblob   # (repeats x n_frames_trial) matrix of frame timestamps in seconds
+    frames_indices                  : longblob   # (repeats x n_frames_trial) matrix of frame indices in tiff file
+    filename_processed              : varchar(512)  # name of the pre-processed file, relative to the session folder
+    """
